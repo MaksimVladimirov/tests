df1938a34bc44f60fc6e0ef5d7d7aced0a5d68da
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-05-22 20:52:14 +0300

    fix

--- src/components/Redux/counterSlice.tsx
+++ src/components/Redux/counterSlice.tsx
@@ -25,5 +25,5 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement,  } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
 export default counterSlice.reducer;
